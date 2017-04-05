5ef0d0eb926a014d84456d5ce666689b833e2003
Carlos <cga.xtr@gmail.com>
2017-04-05 10:07:43 +0200

    static web

---
+++ cabecera.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Melomania</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">Inicio</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
