3f662a1f9acda3e0c78edd9e3e7d871e80586e72
Carlos <cga.xtr@gmail.com>
2017-04-20 13:26:07 +0200

    added modal form to send new messages

--- mensajes.php
+++ mensajes.php
@@ -14,13 +14,44 @@
   </head>
   <body>
     <?php include ('cabecera.php');?>
-
-
 <div class="wrapper">
+
+  <div class="message" data-toggle="modal" data-target="#myModal">
+    <img src="./images/send-button.png" ></img>
+    <p>Mandar nuevo mensaje</p>
+  </div>
+
+  <!-- Modal -->
+  <div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Nuevo mensaje</h4>
+        </div>
+        <div class="modal-body">
+          <label for="destinatary">Destinatario</label>
+          <input type="text" class="form-control" id="destinatary" placeholder="Destinatario">
+          <label for="title">Titulo</label>
+          <input type="text" class="form-control" id="title" placeholder="Titulo">
+          <label for="text">Cuerpo mensaje</label>
+          <textarea class="form-control" id="text" placeholder="Texto"></textarea>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  <!-- Modal content final-->
+
 <div class="list-group">
   <a href="#" class="list-group-item active">
     <h4 class="list-group-item-heading">Titulo</h4>
-    <h6 class="author">autor</h6>
+    <h6 class="author">De:</h6>
     <p class="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </a>
   <a href="#" class="list-group-item">
